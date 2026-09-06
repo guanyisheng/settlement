fb23af8b0671e8bf36b6e2142e9d57e208ce5d93
guanyisheng <208835232+guanyisheng@users.noreply.github.com>
2026-09-06 13:54:57 +0800

    feat: unify staff personal center for profile media and pay QR

--- staff/partials/user-chip.php
+++ staff/partials/user-chip.php
@@ -5,8 +5,8 @@ $initial = mb_substr($user['nickname'] ?: $user['username'], 0, 1);
 ?>
 <div class="user-chip">
     <div class="avatar"><?= e($initial) ?></div>
-    <a href="/staff/password.php" class="logout-btn">
-        <?= svgIcon('password', 'chip-icon') ?><span>资料</span>
+    <a href="/staff/profile.php" class="logout-btn">
+        <?= svgIcon('staff', 'chip-icon') ?><span>我的</span>
     </a>
     <a href="/logout.php" class="logout-btn">
         <?= svgIcon('logout', 'chip-icon') ?><span>退出</span>
